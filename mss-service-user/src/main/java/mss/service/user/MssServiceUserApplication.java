e742416e683011f70f69ad46bee623e4e582c551
huxuanchen <wudianchatou@aliyun.com>
2020-06-24 13:39:31 +0800

    springboot 2.2.8 hello world

---
+++ mss-service-user/src/main/java/mss/service/user/MssServiceUserApplication.java
@@ -0,0 +1,13 @@
+package mss.service.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MssServiceUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MssServiceUserApplication.class, args);
+	}
+
+}
